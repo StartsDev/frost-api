93d3a0811b0e718cfad2bb4afd3c200fd37a98aa
Orlando Rodriguez <orlandog79@gmail.com>
2023-08-21 19:49:51 -0500

    build folder updated

--- build/services/auth.services.js
+++ build/services/auth.services.js
@@ -104,7 +104,7 @@ const loginUserServ = (user) => __awaiter(void 0, void 0, void 0, function* () {
             lastName: foundUser.lastName,
             email: foundUser.email,
         }, secretKey, {
-            expiresIn: "3h",
+            expiresIn: "30d",
         });
         return {
             msg: "Usuario logueado satisfactoriamente...",
