8b839ece19edd74804a03c3e23fd5bc47e1d90ad
Orlando Rodriguez <orlandog79@gmail.com>
2023-08-25 16:53:48 -0500

    routes

--- routes/roleRoutes.ts
+++ routes/roleRoutes.ts
@@ -10,7 +10,7 @@ import { verifyToken, isSuperUser } from "../middleware/authjwt";
 const router = Router();
 
 // Register role
-router.post("/create", verifyToken, isSuperUser, createRole);
+router.post("/create", /**verifyToken*/ /*isSuperUser*/ createRole);
 
 // Get roles
 router.get("/get-roles", getRoles);
