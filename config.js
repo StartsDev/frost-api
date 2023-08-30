55cd8b26665e21034082d536184f8d78ba5623c6
ramirogrisales <ramirogrisales@gmail.com>
2023-08-29 23:42:26 -0500

    prueba

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const configParams = {
   PORT: "8080",
-  DATABASE_URL: "postgres://postgres:R4m1r0.8489@localhost/aire",
+  DATABASE_URL: "postgres://postgres:R4m1r0.8489@localhost:80/aire",
   SECRET_JWT: "Z4hbl80aq45",
   JWT_SECRET_RESET: "lq1lB7oz8Mq1L5XmWTxI6CTNkOKnLjRF68kDpUgJZw",
   HOST_GMAIL: "smtp.gmail.com",
